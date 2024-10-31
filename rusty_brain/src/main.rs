7ca1ac1ed5bdde2682046f617c5d6a6249a1c4ee
Yosif Soliman <yosifsoli981@gmail.com>
2024-10-31 20:48:50 +0300

    SCRUM-104 fix en passant optimization

--- rusty_brain/src/main.rs
+++ rusty_brain/src/main.rs
@@ -2,14 +2,7 @@ use rusty_brain::{bitboards::{self, Bitboards}, board::{self, Board}};
 
 fn main() {
     // println!("Core Engine");
-     let mut board = Board::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 0".to_string());
-    board.print_board();
-    let moves = board.generate_legal_moves();
-    for m in &moves {
-        println!("move {}", m);
-    }
-    board.make_move(moves[2]);
-    
+     let mut board = Board::from_fen("7k/8/1K1r2n1/5P2/8/8/8/8 w - - 0 1".to_string());
     board.print_board();
     let moves = board.generate_legal_moves();
     for m in &moves {
