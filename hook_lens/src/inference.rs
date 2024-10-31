481c9c71d6f11e67edc5423ac4af798b1beb454a
Badr <mohammadhani912@gmail.com>
2024-10-31 22:30:39 +0300

    SCRUM-48 Canny Edge detection

--- hook_lens/src/inference.rs
+++ hook_lens/src/inference.rs
@@ -1,10 +1,15 @@
 use burn::prelude::Backend;
 use image::DynamicImage;
+use imageproc::edges::canny;
 
 pub fn infer/*<B: Backend>*/(/*artifact_dir: &str,  device: B::Device, */board: DynamicImage) {
+    
     // apply canny edge
-
-    board.save("hook_lens\\canny_board.png");
+    let luma_board = board.to_luma8();
+    let low_threshold = 1.0;
+    let high_threshold = 50.0;
+    let canny_board = canny(&luma_board, low_threshold, high_threshold);
+    canny_board.save("hook_lens\\canny_board.png");
 
     // apply hough line detection
 
