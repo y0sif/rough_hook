06a8754613791065762dbe6f05a1937aca8bc3f0
Youssef Zahran <yusef.a.zahran@gmail.com>
2024-12-08 14:07:22 +0200

    add uci to main

--- rusty_brain/src/main.rs
+++ rusty_brain/src/main.rs
@@ -1,17 +1,19 @@
-use rusty_brain::{bitboards::{self, Bitboards}, board::{self, Board}};
+use rusty_brain::{bitboards::{self, Bitboards}, board::{self, Board}, uci::Uci};
 use rusty_brain::square::Square;
 use rusty_brain::transposition::TranspositionTable;
 fn main() {
 
-    let mut board: Board = Board::new();
-    let mut transposition_table: TranspositionTable = TranspositionTable::init();
-    let depth = 6;
-    let maximum_moves = 5;
+    // let mut board: Board = Board::new();
+    // let mut transposition_table: TranspositionTable = TranspositionTable::init();
+    // let depth = 6;
+    // let maximum_moves = 5;
 
-    while board.checkmate != true && board.draw != true && board.move_log.len() <= maximum_moves {
-        let best = board.find_best_move(&mut transposition_table, depth);
-        board.make_move(best.0);
-        print!("{}{} ", Square::from(best.0.get_from()), Square::from(best.0.get_to()));
-    }
-    println!("\nGame ended");
+    // while board.checkmate != true && board.draw != true && board.move_log.len() <= maximum_moves {
+    //     let best = board.find_best_move(&mut transposition_table, depth);
+    //     board.make_move(best.0);
+    //     print!("{}{} ", Square::from(best.0.get_from()), Square::from(best.0.get_to()));
+    // }
+    // println!("\nGame ended");
+    let mut uci = Uci::new();
+    uci.listen();
 }
