c1d2390489c3995662415be47b9f23568f68da71
y0sif <yosifsoli981@gmail.com>
2025-01-27 21:14:26 +0200

    cuda trained kan mode

--- hook_lens/src/main.rs
+++ hook_lens/src/main.rs
@@ -1,9 +1,10 @@
 use burn::{backend::{Autodiff, Wgpu}, optim::AdamConfig};
 use hook_lens::data_and_model::training::{train, TrainingConfig};
+use burn_cuda::{CudaDevice, Cuda};
 
 fn main() {
-    let device = burn::backend::wgpu::WgpuDevice::BestAvailable;
-    train::<Autodiff<Wgpu>>(
+    let device = CudaDevice::default();
+    train::<Autodiff<Cuda<f32, i32>>>(
         TrainingConfig::new(AdamConfig::new()),
         device
     );
