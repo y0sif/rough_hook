4a0b6099f76574b0a4183c0c48dc34f6f9253641
Youssef Zahran <yusef.a.zahran@gmail.com>
2025-01-28 11:35:16 +0200

    add uci

--- rusty_brain/src/main.rs
+++ rusty_brain/src/main.rs
@@ -1,17 +1,6 @@
-use rusty_brain::{bitboards::{self, Bitboards}, board::{self, Board}};
-use rusty_brain::square::Square;
-use rusty_brain::transposition::TranspositionTable;
+use rusty_brain::uci::Uci;
+
 fn main() {
-
-    let mut board: Board = Board::new();
-    let mut transposition_table: TranspositionTable = TranspositionTable::init();
-    let depth = 6;
-    let maximum_moves = 5;
-
-    while board.checkmate != true && board.draw != true && board.move_log.len() <= maximum_moves {
-        let best = board.find_best_move(&mut transposition_table, depth);
-        board.make_move(best.0);
-        print!("{}{} ", Square::from(best.0.get_from()), Square::from(best.0.get_to()));
-    }
-    println!("\nGame ended");
+    let mut uci = Uci::new();
+    uci.listen();
 }
