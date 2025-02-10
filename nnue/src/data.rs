7762d0513ba88c5ac69791eab543d7b96e7a9f22
y0sif <yosifsoli981@gmail.com>
2025-02-10 16:48:11 +0200

    adjust data loading

--- nnue/src/data.rs
+++ nnue/src/data.rs
@@ -11,95 +11,16 @@ use burn_dataset::SqliteDataset;
 use nn::Sigmoid;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
+use std::path::Path;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct ChessPositionRaw {
-    pub fen: String,
-    #[serde(rename = "cp")]
-    pub evaluation: Option<f32>,
-}
-
-#[derive(Clone, Debug)]
 pub struct ChessPositionItem{
     pub fen_to_pieces: Vec<i8>,
     pub evaluation: f32,
 } 
 
-pub struct RawToItem;
 
-impl Mapper<ChessPositionRaw, ChessPositionItem> for RawToItem{
-    fn map(&self, item: &ChessPositionRaw) -> ChessPositionItem {
-        /*
-        White
-        king
-        pawn
-        knight
-        bishop
-        rook
-        queen
-        Black
-        king
-        pawn
-        knight
-        bishop
-        rook
-        queen
-         */
-        let map = HashMap::from([
-            ('K', 0),
-            ('P', 1),
-            ('N', 2),
-            ('B', 3),
-            ('R', 4),
-            ('Q', 5),
-            ('k', 6),
-            ('p', 7),
-            ('n', 8),
-            ('b', 9),
-            ('r', 10),
-            ('q', 11),
-        ]);
-        let mut position = [[0; 64]; 12];
-        let fen_str: Vec<&str> = item.fen.split_whitespace().collect();
-        let mut count: usize = 0;
-        for piece in fen_str[0].chars(){
-            if piece == '/'{
-                continue;
-            }
-            if piece.is_numeric(){
-                count += piece.to_digit(10).unwrap() as usize;
-                continue;
-            }
-            if let Some(value) = map.get(&piece){
-                position[*value][count] = 1;
-                count += 1
-            }
-        }
-        let position: Vec<i8> = position.into_iter()
-                                .flat_map(|item| item).collect();
-        ChessPositionItem {
-            fen_to_pieces: position,
-            evaluation: match item.evaluation {
-                Some(val) => val,
-                None => {
-                    match fen_str[1]{
-                        "w" => {
-                            20000.0
-                        },
-                        "b" => {
-                            -20000.0
-                        },
-                        _ => {
-                            0.0
-                        },
-                    }
-                }
-            }
-        }
-    }
-}
-
-type MappedDataset = MapperDataset<InMemDataset<ChessPositionRaw>, RawToItem, ChessPositionRaw>;
+type MappedDataset = Box<dyn Dataset<ChessPositionItem>>;
 pub struct ChessPositionDataSet{
     dataset: MappedDataset,
 }
@@ -126,16 +47,12 @@ impl ChessPositionDataSet {
     }
 
     fn new(split: &str) -> Self{
-        println!("dataloading started");
-        type ChessEval = SqliteDataset<ChessPositionRaw>;
-        let root: SqliteDataset<ChessPositionRaw> = HuggingfaceDatasetLoader::new("Lichess/chess-evaluations")
-            .dataset("train") // The training split.
-            .unwrap();
-
-        println!("dataloading finished");
-
-        let dataset = ShuffledDataset::<ChessEval,ChessPositionRaw>::with_seed(root, 42);
-        type PartialData = PartialDataset<ShuffledDataset<ChessEval, ChessPositionRaw>, ChessPositionRaw>;
+        let db_file = Path::new("nnue/data_in_sql_lite/chess_positions_nnue.db");
+        
+        let dataset = SqliteDataset::from_db_file(db_file, split).unwrap();
+        
+        let dataset= ShuffledDataset::<SqliteDataset<ChessPositionItem>, ChessPositionItem>::with_seed(dataset, 42);
+        type PartialData = PartialDataset<ShuffledDataset::<SqliteDataset<ChessPositionItem>, ChessPositionItem>, ChessPositionItem>;
 
         let data_split = match split {
             "train" => PartialData::new(dataset, 0, 800000), // Get first 80% dataset
@@ -143,23 +60,11 @@ impl ChessPositionDataSet {
             _ => panic!("Invalid split type"),                     // Handle unexpected split types
         };
 
-        println!("data spliting done");
-        let mut fens = Vec::new();
-        let mut evals = Vec::new();
-        for item in data_split.iter(){
-            fens.push(item.fen);
-            evals.push(item.evaluation);
+        let dataset = Box::new(data_split);
+
+        ChessPositionDataSet {
+            dataset
         }
-
-        let items: Vec<_> = fens
-            .into_iter()
-            .zip(evals)
-            .map(|(fen, eval)| ChessPositionRaw { fen:fen, evaluation:eval})
-            .collect();
-        let dataset = InMemDataset::new(items);
-        let dataset = MapperDataset::new(dataset, RawToItem);
-
-        ChessPositionDataSet { dataset: dataset}
     }
 }
 
