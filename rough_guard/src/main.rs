0abd9183922a4437aa4f66b27e2b75612884bca8
Khaled Ayman <95758602+Khaled-Ayman1@users.noreply.github.com>
2025-03-16 02:55:45 +0200

    Update main.rs

--- rough_guard/src/main.rs
+++ rough_guard/src/main.rs
@@ -11,23 +11,23 @@ fn main() {
     let artifact_dir = "/tmp/rough_guard";
 
     //Wgpu Code
-    type MyBackend = Wgpu<f32, i32>;
-    type MyAutodiffBackend = Autodiff<MyBackend>;
-    let device = burn::backend::wgpu::WgpuDevice::BestAvailable;
-    rough_guard::training::train::<Autodiff<MyAutodiffBackend>>(
-        artifact_dir,
-        TrainingConfig::new(ModelConfig::new(), AdamConfig::new()),
-        device.clone()
-    );
+    //type MyBackend = Wgpu<f32, i32>;
+    //type MyAutodiffBackend = Autodiff<MyBackend>;
+    //let device = burn::backend::wgpu::WgpuDevice::BestAvailable;
+    //rough_guard::training::train::<Autodiff<MyAutodiffBackend>>(
+    //    artifact_dir,
+    //    TrainingConfig::new(ModelConfig::new(), AdamConfig::new()),
+    //    device.clone()
+    //);
     
 
     // CUDA CODE
-    //let device = CudaDevice::default();
-    //  rough_guard::training::train::<Autodiff<Cuda<f32, i32>>>(
-    //      artifact_dir,
-    //      TrainingConfig::new(ModelConfig::new(), AdamConfig::new()),
-    //      device.clone(),
-    //  );
+    let device = CudaDevice::default();
+      rough_guard::training::train::<Autodiff<Cuda<f32, i32>>>(
+          artifact_dir,
+          TrainingConfig::new(ModelConfig::new(), AdamConfig::new()),
+          device.clone(),
+      );
 
     // TEST DB
     //data::test_deserialization();
