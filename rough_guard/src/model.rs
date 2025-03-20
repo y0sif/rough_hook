0412056a6d1e8c9631bac2363e65e03fb11dd056
y0sif <yosifsoli981@gmail.com>
2025-03-20 11:19:52 +0200

    train two kan layers

--- rough_guard/src/model.rs
+++ rough_guard/src/model.rs
@@ -7,7 +7,8 @@ use burn_efficient_kan::{Kan as EfficientKan, KanOptions};
 
 #[derive(Module, Debug)]
 pub struct Model<B: Backend> {
-    pub kan_layer: EfficientKan<B>,
+    pub kan_layer1: EfficientKan<B>,
+    pub kan_layer2: EfficientKan<B>,
     pub class_weights: Tensor<B, 1>
 }
 
@@ -20,9 +21,14 @@ impl ModelConfig {
         B::FloatElem: ndarray_linalg::Scalar + ndarray_linalg::Lapack, 
     {
         Model{
-            kan_layer: EfficientKan::new(&KanOptions::new([
+            kan_layer1: EfficientKan::new(&KanOptions::new([
                 241,
+                256,
+                128
+            ]).with_grid_size(6).with_spline_order(6), device),
+            kan_layer2: EfficientKan::new(&KanOptions::new([
                 128,
+                64,
                 4
             ]).with_grid_size(6).with_spline_order(6), device),
             class_weights
@@ -32,8 +38,8 @@ impl ModelConfig {
 
 impl<B: Backend> Model<B> {
     pub fn forward(&self, games: Tensor<B, 2>) -> Tensor<B, 2> {
-        let x = self.kan_layer.forward(games);
-        x
+        let x = self.kan_layer1.forward(games);
+        self.kan_layer2.forward(x)
     }
 
     pub fn infer(&self, games: Tensor<B, 2>) -> Tensor<B, 2> {
