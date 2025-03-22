44266c289eea6b5936ead8139732a40c879a0814
y0sif <yosifsoli981@gmail.com>
2025-03-22 15:23:16 +0200

    add cuda backend

--- rusty_brain/src/main.rs
+++ rusty_brain/src/main.rs
@@ -2,17 +2,18 @@ use rusty_brain::uci;
 use nnue::training::TrainingConfig;
 use burn::{backend::wgpu::WgpuDevice, config::Config, module::Module, record::{CompactRecorder, Recorder}};
 use burn::backend::Wgpu;
+use burn_cuda::{CudaDevice, Cuda};
 fn main() {
-    let device = WgpuDevice::default();
-    let artifact_dir = "/tmp/nnue_8";
+    let device = CudaDevice::new(0);
+    let artifact_dir = "/home/y0sif/nnue_models/nnue_16";
     let config = TrainingConfig::load(format!("{artifact_dir}/config.json"))
         .expect("Config should exist for the model");
     let record = CompactRecorder::new()
         .load(format!("{artifact_dir}/model").into(), &device)
         .expect("Trained model should exist");
 
-    let model = config.model.init::<Wgpu<f32, i32>>(&device).load_record(record);
+    let model = config.model.init::<Cuda<f32, i32>>(&device).load_record(record);
 
-    let mut uci = uci::Uci::<Wgpu<f32, i32>>::new(model, device.clone());
+    let mut uci = uci::Uci::<Cuda<f32, i32>>::new(model, device.clone());
     uci.listen();
 }
