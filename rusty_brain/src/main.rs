3ab7b7b6b57ae7273d1d9720181bdc98ad164814
y0sif <yosifsoli981@gmail.com>
2025-04-15 17:20:50 +0200

    add ndarray backend to inference

--- rusty_brain/src/main.rs
+++ rusty_brain/src/main.rs
@@ -1,19 +1,19 @@
 use rusty_brain::uci;
 use kanue::training::TrainingConfig;
-use burn::{backend::wgpu::WgpuDevice, config::Config, module::Module, record::{CompactRecorder, Recorder}};
-use burn::backend::Wgpu;
+use burn::{backend::{ndarray::NdArrayDevice, wgpu::WgpuDevice}, config::Config, module::Module, record::{CompactRecorder, Recorder}};
+use burn::backend::NdArray;
 use burn_cuda::{CudaDevice, Cuda};
 fn main() {
-    let device = CudaDevice::new(0);
-    let artifact_dir = "/home/y0sif/kanue_models/kanue_1024";
+    let device = NdArrayDevice::Cpu;
+    let artifact_dir = "/home/y0sif/kanue_models/kanue_8";
     let config = TrainingConfig::load(format!("{artifact_dir}/config.json"))
         .expect("Config should exist for the model");
     let record = CompactRecorder::new()
         .load(format!("{artifact_dir}/model").into(), &device)
         .expect("Trained model should exist");
 
-    let model = config.model.init::<Cuda<f32, i32>>(&device).load_record(record);
+    let model = config.model.init::<NdArray<f32, i8>>(&device).load_record(record);
 
-    let mut uci = uci::Uci::<Cuda<f32, i32>>::new(model, device.clone());
+    let mut uci = uci::Uci::<NdArray<f32, i8>>::new(model, device.clone());
     uci.listen();
 }
